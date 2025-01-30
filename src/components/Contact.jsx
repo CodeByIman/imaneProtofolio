b3ab41b3bacc3df4dc86345103b08c097805d9a2
pc <imanebouboul0@gmail.com>
2025-01-30 17:25:11 +0100

    6

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <section className="p-10 bg-gray-900 text-white text-center">
       <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
       <p>📧 Email: your.email@example.com</p>
-      <p>🔗 LinkedIn: <a href="https://www.linkedin.com/in/imane-bouboul/" className="text-blue-400">Your Profile</a></p>
-      <p>💻 GitHub: <a href="https://github.com/CodeByIman" className="text-blue-400">Your GitHub</a></p>
+      <p>🔗 LinkedIn: <a href="https://www.linkedin.com/in/imane-bouboul/" className="text-blue-400">My linkdin Profile</a></p>
+      <p>💻 GitHub: <a href="https://github.com/CodeByIman" className="text-blue-400">My GitHub Profile</a></p>
     </section>
   );
 };
